5add425285c01b64e33c69f0af41315a8c3684a0
unknown <Christian-Encalada>
2025-01-05 21:00:58 -0500

    Implement CRUD for Groups entity with Swagger documentation

--- src/groups/entities/group.entity.ts
+++ src/groups/entities/group.entity.ts
@@ -1,23 +1,17 @@
-import { Team } from "src/teams/entities/team.entity";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Team } from 'src/teams/entities/team.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'groups' })
 export class Group {
-
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column('text', {
     unique: true,
-    nullable: false
+    nullable: false,
   })
   groupName: string;
 
-  @OneToMany(
-    () => Team,
-    (team) => team.group,
-    { cascade: true },
-  )
+  @OneToMany(() => Team, (team) => team.group, { cascade: true })
   team: Team;
-
 }
--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,36 +1,99 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  Query,
+} from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Role } from 'src/users/entities/user.entity';
 
-@ApiTags('Groups not available!')
+@ApiTags('Groups')
 @Controller('groups')
 export class GroupsController {
   constructor(private readonly groupsService: GroupsService) {}
 
   @Post()
+  @Auth(Role.ADMIN)
+  @ApiOperation({ summary: 'Create a group' })
+  @ApiBody({ type: CreateGroupDto })
   create(@Body() createGroupDto: CreateGroupDto) {
     return this.groupsService.create(createGroupDto);
   }
 
   @Get()
-  findAll() {
-    return this.groupsService.findAll();
+  @Auth(Role.ADMIN)
+  @ApiOperation({ summary: 'Obtain all groups' })
+  @ApiQuery({
+    name: 'limit',
+    description: 'Number of results per page',
+    required: false,
+    type: Number,
+    default: 10,
+  })
+  @ApiQuery({
+    name: 'page',
+    description: 'What page do you need',
+    required: false,
+    type: Number,
+    default: 1,
+  })
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.groupsService.findAll(paginationDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.groupsService.findOne(+id);
+  @Auth(Role.ADMIN)
+  @ApiOperation({ summary: 'Obtain one group' })
+  @ApiParam({
+    name: 'id',
+    description: 'The id of the group to obtain',
+    type: String,
+  })
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.groupsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto) {
-    return this.groupsService.update(+id, updateGroupDto);
+  @Auth(Role.ADMIN)
+  @ApiOperation({ summary: 'Update a group' })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description: 'Group ID to be updated (UUID)',
+  })
+  @ApiBody({ type: UpdateGroupDto })
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateGroupDto: UpdateGroupDto,
+  ) {
+    return this.groupsService.update(id, updateGroupDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.groupsService.remove(+id);
+  @Auth(Role.ADMIN)
+  @ApiOperation({ summary: 'Remove a group' })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description: 'Group ID to be removed (UUID)',
+  })
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.groupsService.remove(id);
   }
 }
