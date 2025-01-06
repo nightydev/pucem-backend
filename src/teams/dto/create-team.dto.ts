843b47927a406017c39830c5ba3cfe673c8227ef
unknown <Christian-Encalada>
2025-01-05 21:03:26 -0500

    Implement CRUD for Teams entity with Swagger documentation

--- src/teams/dto/create-team.dto.ts
+++ src/teams/dto/create-team.dto.ts
@@ -1,1 +1,25 @@
-export class CreateTeamDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUUID } from 'class-validator';
+
+export class CreateTeamDto {
+  @ApiProperty({
+    description: `Team name`,
+    example: 'Team A',
+  })
+  @IsString()
+  teamName: string;
+
+  @ApiProperty({
+    description: `Group ID`,
+    example: 'uuid-of-group',
+  })
+  @IsUUID()
+  groupId: string;
+
+  @ApiProperty({
+    description: `Patient ID`,
+    example: 'uuid-of-patient',
+  })
+  @IsUUID()
+  patientId: string;
+}
