c5228bde93ced738849f9abc56fa50f1de8de0ce
Christian-Encalada <cris_7715@hotmail.com>
2025-02-20 01:13:20 -0500

    fix

--- src/consultation/consultation.controller.ts
+++ src/consultation/consultation.controller.ts
@@ -1,32 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { ConsultationService } from './consultation.service';
 import { CreateConsultationInitialDto } from './dto/create-consultation-initial.dto';
 import { CreateConsultationSubsequentDto } from './dto/create-consultation-subsequent.dto';
 import { ApiBody, ApiOperation, ApiParam } from '@nestjs/swagger';
-import { Auth } from '../auth/decorators/auth.decorator';
-import { Role } from '../auth/entities/role.entity';
 
 @Controller('consultations')
 export class ConsultationController {
-  constructor(private readonly consultationService: ConsultationService) { }
+  constructor(private readonly consultationService: ConsultationService) {}
 
   @Post('initial')
   @ApiOperation({ summary: 'Create a consultation initial' })
   @ApiBody({ type: CreateConsultationInitialDto })
-  createInitial(@Body() createConsultationInitialDto: CreateConsultationInitialDto) {
+  createInitial(
+    @Body() createConsultationInitialDto: CreateConsultationInitialDto,
+  ) {
     return this.consultationService.createInitial(createConsultationInitialDto);
   }
 
   @Post('subsequent')
-  @ApiOperation({ summary: 'Create a consultation subsequent based in a initial' })
+  @ApiOperation({
+    summary: 'Create a consultation subsequent based in a initial',
+  })
   @ApiBody({ type: CreateConsultationSubsequentDto })
-  createSubsequent(@Body() createConsultationSubsequentDto: CreateConsultationSubsequentDto) {
-    return this.consultationService.createSubsequent(createConsultationSubsequentDto);
+  createSubsequent(
+    @Body() createConsultationSubsequentDto: CreateConsultationSubsequentDto,
+  ) {
+    return this.consultationService.createSubsequent(
+      createConsultationSubsequentDto,
+    );
   }
 
   @Get('lastsubsequent/:id')
   @ApiOperation({ summary: 'Obtain the last subsequent based in a initial ID' })
-  @ApiParam({ name: 'id', type: String, description: 'ID of the consultation initial to be searched its last subsequent one, lol so hard' })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description:
+      'ID of the consultation initial to be searched its last subsequent one, lol so hard',
+  })
   findLastSubsequent(@Param('id', ParseUUIDPipe) id: string) {
     return this.consultationService.findLastSubsequent(id);
   }
@@ -37,7 +56,9 @@ export class ConsultationController {
   }
 
   @Get('user/:userId')
-  @ApiOperation({ summary: 'Obtener todas las consultas de un usuario específico' })
+  @ApiOperation({
+    summary: 'Obtener todas las consultas de un usuario específico',
+  })
   @ApiParam({ name: 'userId', type: String, description: 'ID del usuario' })
   findAllByUser(@Param('userId', ParseUUIDPipe) userId: string) {
     return this.consultationService.findAllByUser(userId);
