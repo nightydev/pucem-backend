3d88597d0851a8b851b2fcdf18f1c4189278fd65
Byron Serrano <bsantyserrano@gmail.com>
2025-08-28 23:54:31 -0500

    Enhance CreateNeurologicaDto with detailed pain records and body map structure

--- src/neurologica/dto/create-neurologica.dto.ts
+++ src/neurologica/dto/create-neurologica.dto.ts
@@ -8,40 +8,50 @@ import {
   ValidateNested,
   Min,
   IsArray,
+  IsObject,
+  IsNumber,
 } from 'class-validator';
 import { Type } from 'class-transformer';
 
 class AlteracionesMarchaDto {
   @ApiProperty({ description: 'Marcha de Trendelenburg', example: false })
-  @IsBoolean() @IsOptional()
+  @IsBoolean()
+  @IsOptional()
   marchaTrendelenburg?: boolean;
 
   @ApiProperty({ description: 'Marcha en tuerca', example: false })
-  @IsBoolean() @IsOptional()
+  @IsBoolean()
+  @IsOptional()
   marchaTuerca?: boolean;
 
   @ApiProperty({ description: 'Marcha atáxica', example: false })
-  @IsBoolean() @IsOptional()
+  @IsBoolean()
+  @IsOptional()
   marchaAtaxica?: boolean;
 
   @ApiProperty({ description: 'Marcha en segador', example: false })
-  @IsBoolean() @IsOptional()
+  @IsBoolean()
+  @IsOptional()
   marchaSegador?: boolean;
 
   @ApiProperty({ description: 'Marcha en tijeras', example: false })
-  @IsBoolean() @IsOptional()
+  @IsBoolean()
+  @IsOptional()
   marchaTijeras?: boolean;
 
   @ApiProperty({ description: 'Marcha tabética', example: false })
-  @IsBoolean() @IsOptional()
+  @IsBoolean()
+  @IsOptional()
   marchaTabetica?: boolean;
 
   @ApiProperty({ description: 'Marcha coreica', example: false })
-  @IsBoolean() @IsOptional()
+  @IsBoolean()
+  @IsOptional()
   marchaCoreica?: boolean;
 
   @ApiProperty({ description: 'Marcha distónica', example: false })
-  @IsBoolean() @IsOptional()
+  @IsBoolean()
+  @IsOptional()
   marchaDistonica?: boolean;
 
   @ApiProperty({
@@ -49,7 +59,8 @@ class AlteracionesMarchaDto {
     example: 'Descripción de otras alteraciones',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   otrasAlteraciones?: string;
 }
 
@@ -59,7 +70,8 @@ class RiesgoCaidaDto {
     example: '15 segundos',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   tiempoTimedUpGo?: string;
 
   @ApiProperty({
@@ -67,7 +79,8 @@ class RiesgoCaidaDto {
     example: 'moderado',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   riesgoEvaluado?: string;
 
   @ApiProperty({
@@ -75,42 +88,179 @@ class RiesgoCaidaDto {
     example: 'Comentarios sobre el riesgo de caída',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   comentariosRiesgo?: string;
 }
 
 class BarthelIndexDto {
-  @ApiProperty({ description: 'Puntuación Barthel - Vestirse', example: 10, required: false })
-  @IsInt() @IsOptional()
+  @ApiProperty({
+    description: 'Puntuación Barthel - Vestirse',
+    example: 10,
+    required: false,
+  })
+  @IsInt()
+  @IsOptional()
   vestirse?: number;
 
-  @ApiProperty({ description: 'Puntuación Barthel - Arreglarse', example: 5, required: false })
-  @IsInt() @IsOptional()
+  @ApiProperty({
+    description: 'Puntuación Barthel - Arreglarse',
+    example: 5,
+    required: false,
+  })
+  @IsInt()
+  @IsOptional()
   arreglarse?: number;
 
-  @ApiProperty({ description: 'Puntuación Barthel - Deposición', example: 10, required: false })
-  @IsInt() @IsOptional()
+  @ApiProperty({
+    description: 'Puntuación Barthel - Deposición',
+    example: 10,
+    required: false,
+  })
+  @IsInt()
+  @IsOptional()
   deposicion?: number;
 
-  @ApiProperty({ description: 'Puntuación Barthel - Micción', example: 10, required: false })
-  @IsInt() @IsOptional()
+  @ApiProperty({
+    description: 'Puntuación Barthel - Micción',
+    example: 10,
+    required: false,
+  })
+  @IsInt()
+  @IsOptional()
   miccion?: number;
 
-  @ApiProperty({ description: 'Puntuación Barthel - Uso del retrete', example: 10, required: false })
-  @IsInt() @IsOptional()
+  @ApiProperty({
+    description: 'Puntuación Barthel - Uso del retrete',
+    example: 10,
+    required: false,
+  })
+  @IsInt()
+  @IsOptional()
   usoRetrete?: number;
 
-  @ApiProperty({ description: 'Puntuación Barthel - Trasladarse', example: 15, required: false })
-  @IsInt() @IsOptional()
+  @ApiProperty({
+    description: 'Puntuación Barthel - Trasladarse',
+    example: 15,
+    required: false,
+  })
+  @IsInt()
+  @IsOptional()
   trasladarse?: number;
 
-  @ApiProperty({ description: 'Puntuación Barthel - Deambular', example: 15, required: false })
-  @IsInt() @IsOptional()
+  @ApiProperty({
+    description: 'Puntuación Barthel - Deambular',
+    example: 15,
+    required: false,
+  })
+  @IsInt()
+  @IsOptional()
   deambular?: number;
 
-  @ApiProperty({ description: 'Puntuación Barthel - Escaleras', example: 10, required: false })
-  @IsInt() @IsOptional()
+  @ApiProperty({
+    description: 'Puntuación Barthel - Escaleras',
+    example: 10,
+    required: false,
+  })
+  @IsInt()
+  @IsOptional()
   escaleras?: number;
+}
+class BodyMapDto {
+  @ApiProperty({
+    description: 'Mapa corporal con ID de parte y nivel de dolor',
+    // eslint-disable-next-line prettier/prettier
+    example: {
+      id_parte_cabeza: {
+        nivel: 5,
+        description: 'Dolor intenso',
+      },
+      id_parte_torso: {
+        nivel: 3,
+        description: 'Dolor moderado',
+      },
+      id_parte_brazo_derecho: {
+        nivel: 2,
+        description: 'Dolor leve',
+      },
+    },
+  })
+  @IsObject()
+  @IsOptional()
+  bodyMap?: { [key: string]: { nivel: number; description: string } }; // ID de parte como string y nivel como number
+}
+
+class DolorRegistroDto {
+  @ApiProperty({
+    description: 'Región corporal afectada',
+    example: 'lumbar',
+  })
+  @IsString()
+  region: string;
+
+  @ApiProperty({
+    description: 'Temporalidad del dolor',
+    example: 'crónico',
+  })
+  @IsString()
+  tiempo: string;
+
+  @ApiProperty({
+    description: 'Si el dolor se irradia',
+    example: true,
+  })
+  @IsBoolean()
+  irradiado: boolean;
+
+  @ApiProperty({
+    description: 'Tipo de dolor',
+    example: 'punzante',
+  })
+  @IsString()
+  tipo: string;
+
+  @ApiProperty({
+    description: 'Escala Visual Numérica (EVN)',
+    example: 7,
+  })
+  @IsNumber()
+  evn: number;
+
+  @ApiProperty({
+    description: 'Descripción subjetiva del dolor',
+    example: 'dolor intenso',
+  })
+  @IsString()
+  subjetiva: string;
+
+  @ApiProperty({
+    description: 'Factores que alivian el dolor',
+    example: ['reposo', 'calor'],
+    isArray: true,
+    type: String,
+  })
+  @IsArray()
+  @IsString({ each: true })
+  alivian: string[];
+
+  @ApiProperty({
+    description: 'Factores que agravan el dolor',
+    example: ['movimiento', 'frío'],
+    isArray: true,
+    type: String,
+  })
+  @IsArray()
+  @IsString({ each: true })
+  agravan: string[];
+
+  @ApiProperty({
+    description: 'Comentarios adicionales sobre el dolor',
+    example: 'empeora por las mañanas',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  comentarios?: string;
 }
 
 export class CreateNeurologicaDto {
@@ -123,7 +273,8 @@ export class CreateNeurologicaDto {
   ci: string;
 
   @ApiProperty({ description: 'Edad del paciente', example: 45 })
-  @IsInt() @Min(0)
+  @IsInt()
+  @Min(0)
   edad: number;
 
   @ApiProperty({ description: 'Diagnóstico médico', example: 'Parkinson' })
@@ -139,7 +290,8 @@ export class CreateNeurologicaDto {
     example: 'Antecedentes familiares de la enfermedad',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   antecedentesHeredofamiliares?: string;
 
   @ApiProperty({
@@ -147,7 +299,8 @@ export class CreateNeurologicaDto {
     example: 'Medicamentos actuales',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   antecedentesFarmacologicos?: string;
 
   @ApiProperty({
@@ -155,7 +308,8 @@ export class CreateNeurologicaDto {
     example: 'Historia nutricional del paciente',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   historiaNutricional?: string;
 
   @ApiProperty({
@@ -163,7 +317,8 @@ export class CreateNeurologicaDto {
     example: 'Alergias conocidas',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   alergias?: string;
 
   @ApiProperty({
@@ -171,7 +326,8 @@ export class CreateNeurologicaDto {
     example: 'Tabaquismo, alcoholismo',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   habitosToxicos?: string;
 
   @ApiProperty({
@@ -179,7 +335,8 @@ export class CreateNeurologicaDto {
     example: 'Cirugías previas',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   quirurgico?: string;
 
   @ApiProperty({
@@ -187,7 +344,8 @@ export class CreateNeurologicaDto {
     example: 'Estado de comunicación del paciente',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   comunicacion?: string;
 
   @ApiProperty({
@@ -195,11 +353,13 @@ export class CreateNeurologicaDto {
     example: 'Dolor lumbar intermitente',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   dolor?: string;
 
   @ApiProperty({ description: 'Utiliza silla de ruedas', example: false })
-  @IsBoolean() @IsOptional()
+  @IsBoolean()
+  @IsOptional()
   utilizaSillaRuedas?: boolean;
 
   @ApiProperty({
@@ -207,7 +367,8 @@ export class CreateNeurologicaDto {
     example: 'Descripción de la amnesis',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   amnesis?: string;
 
   @ApiProperty({
@@ -215,7 +376,8 @@ export class CreateNeurologicaDto {
     example: 'Descripción del inicio y evolución',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   inicioEvolucion?: string;
 
   @ApiProperty({
@@ -223,15 +385,82 @@ export class CreateNeurologicaDto {
     example: 'Descripción del entorno familiar',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   entornoFamiliar?: string;
 
-  @ApiProperty({ description: 'Alteraciones de la marcha', type: AlteracionesMarchaDto, required: false })
-  @ValidateNested() @Type(() => AlteracionesMarchaDto) @IsOptional()
+  @ApiProperty({
+    description: 'Mapa corporal con partes seleccionadas y niveles de dolor',
+    // eslint-disable-next-line prettier/prettier
+    example: [
+      {
+        id_parte_cabeza: {
+          nivel: 5,
+          description: 'Dolor intenso',
+        },
+        id_parte_torso: {
+          nivel: 3,
+          description: 'Dolor moderado',
+        },
+        id_parte_brazo_derecho: {
+          nivel: 2,
+          description: 'Dolor leve',
+        },
+      },
+    ],
+    required: false,
+    isArray: true,
+    type: BodyMapDto,
+  })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => BodyMapDto)
+  @IsOptional()
+  bodyMap?: BodyMapDto[];
+
+  @ApiProperty({
+    description: 'Registros detallados de dolor',
+    example: [
+      {
+        region: 'lumbar',
+        tiempo: 'crónico',
+        irradiado: true,
+        tipo: 'punzante',
+        evn: 7,
+        subjetiva: 'dolor intenso',
+        alivian: ['reposo'],
+        agravan: ['movimiento'],
+        comentarios: 'empeora por las mañanas',
+      },
+    ],
+    required: false,
+    isArray: true,
+    type: DolorRegistroDto,
+  })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => DolorRegistroDto)
+  @IsOptional()
+  dolorRegistros?: DolorRegistroDto[];
+
+  @ApiProperty({
+    description: 'Alteraciones de la marcha',
+    type: AlteracionesMarchaDto,
+    required: false,
+  })
+  @ValidateNested()
+  @Type(() => AlteracionesMarchaDto)
+  @IsOptional()
   alteracionesMarcha?: AlteracionesMarchaDto;
 
-  @ApiProperty({ description: 'Riesgo de caída', type: RiesgoCaidaDto, required: false })
-  @ValidateNested() @Type(() => RiesgoCaidaDto) @IsOptional()
+  @ApiProperty({
+    description: 'Riesgo de caída',
+    type: RiesgoCaidaDto,
+    required: false,
+  })
+  @ValidateNested()
+  @Type(() => RiesgoCaidaDto)
+  @IsOptional()
   riesgoCaida?: RiesgoCaidaDto;
 
   @ApiProperty({
@@ -239,7 +468,8 @@ export class CreateNeurologicaDto {
     example: 'Descripción del alcance motor',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   alcanceMotor?: string;
 
   @ApiProperty({
@@ -247,7 +477,8 @@ export class CreateNeurologicaDto {
     example: 'Comentarios del examinador',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   comentariosExaminador?: string;
 
   @ApiProperty({
@@ -255,19 +486,28 @@ export class CreateNeurologicaDto {
     example: 'Resumen de los resultados obtenidos',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   resumenResultados?: string;
 
-  @ApiProperty({ description: 'Índice de Barthel', type: BarthelIndexDto, required: false })
-  @ValidateNested() @Type(() => BarthelIndexDto) @IsOptional()
+  @ApiProperty({
+    description: 'Índice de Barthel',
+    type: BarthelIndexDto,
+    required: false,
+  })
+  @ValidateNested()
+  @Type(() => BarthelIndexDto)
+  @IsOptional()
   barthel?: BarthelIndexDto;
 
   @ApiProperty({
-    description: 'Puntuación total del Índice de Barthel (calculada en backend)',
+    description:
+      'Puntuación total del Índice de Barthel (calculada en backend)',
     example: 95,
     required: false,
   })
-  @IsInt() @IsOptional()
+  @IsInt()
+  @IsOptional()
   barthelTotal?: number;
 
   // =========================
@@ -281,7 +521,8 @@ export class CreateNeurologicaDto {
     isArray: true,
     type: String,
   })
-  @IsArray() @IsOptional()
+  @IsArray()
+  @IsOptional()
   // Nota: si quieres validar cada string, agrega @IsString({ each: true })
   cif?: { codigo: string; descripcion: string }[];
 
@@ -290,7 +531,8 @@ export class CreateNeurologicaDto {
     example: 'Hombro derecho descendido',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   observacionesVistaAnterior?: string;
 
   @ApiProperty({
@@ -298,7 +540,8 @@ export class CreateNeurologicaDto {
     example: 'Escápulas aladas',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   observacionesVistaPosterior?: string;
 
   @ApiProperty({
@@ -306,7 +549,8 @@ export class CreateNeurologicaDto {
     example: 'Cabeza adelantada',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   observacionesVistaLateralDerecha?: string;
 
   @ApiProperty({
@@ -314,7 +558,8 @@ export class CreateNeurologicaDto {
     example: 'Aumento lordosis',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   observacionesVistaLateralIzquierda?: string;
 
   @ApiProperty({
@@ -322,7 +567,8 @@ export class CreateNeurologicaDto {
     example: 'Síndrome doloroso lumbar mecánico',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   diagnosticoFisioterapeutico?: string;
 
   @ApiProperty({
@@ -330,6 +576,7 @@ export class CreateNeurologicaDto {
     example: 'Terapia manual + fortalecimiento core 8 semanas',
     required: false,
   })
-  @IsString() @IsOptional()
+  @IsString()
+  @IsOptional()
   planFisioterapeutico?: string;
 }
